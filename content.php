1886c215c7ed3ec1cd18b2f63374d3cfd634db51
Dennis Buehring <1625376+erSitzt@users.noreply.github.com>
2025-05-22 16:10:20 +0200

    looks

--- content.php
+++ content.php
@@ -4,18 +4,28 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'kb-article-excerpt mb-5 pb-4 border-bottom' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('kb-article-excerpt mb-5 pb-4 border-bottom shadow-sm rounded bg-white'); ?>>
 	<header class="entry-header page-header mb-3">
-		<h2 class="entry-title h4 mb-2"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta text-muted small d-flex align-items-center gap-3 mb-2">
-			<?php ipt_kb_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<div class="d-flex align-items-center mb-2">
+			<div class="flex-shrink-0 me-3 d-none d-md-block">
+				<span class="bg-primary bg-opacity-10 text-primary rounded-circle d-inline-flex align-items-center justify-content-center" style="width:48px;height:48px;font-size:1.5rem;">
+					<i class="fa-solid fa-file-lines"></i>
+				</span>
+			</div>
+			<div class="flex-grow-1">
+				<h2 class="entry-title h4 mb-1 fw-bold"><a href="<?php the_permalink(); ?>" rel="bookmark" class="link-dark text-decoration-none"><?php the_title(); ?></a></h2>
+				<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta text-muted small d-flex align-items-center flex-wrap gap-3">
+					<span><i class="fa-regular fa-calendar me-1"></i> <?php echo get_the_date(); ?></span>
+					<span><i class="fa-regular fa-user me-1"></i> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" class="text-decoration-none text-secondary"><?php the_author(); ?></a></span>
+					<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
+					<span><i class="fa-regular fa-comments me-1"></i> <?php comments_popup_link( __( 'Leave a comment', 'ipt_kb' ), __( '1', 'ipt_kb' ), __( '%', 'ipt_kb' ) ); ?></span>
+					<?php endif; ?>
+				</div>
+				<?php endif; ?>
+			</div>
+		</div>
 	</header><!-- .entry-header -->
-
-	<div class="clearfix"></div>
 
 	<?php if ( has_post_thumbnail() ) : ?>
 	<div class="float-start kb-thumb-medium d-none d-sm-block me-3 mb-2">
@@ -30,45 +40,29 @@
 	</div>
 	<?php endif; ?>
 
-	<div class="entry entry-excerpt">
+	<div class="entry entry-excerpt mb-2">
 		<?php the_excerpt(); ?>
 	</div>
 
-	<div class="clearfix"></div>
 	<footer class="entry-meta mt-2">
-		<p class="d-block d-sm-none mb-2">
-			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary btn-block"><i class="fa-solid fa-arrow-right"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
-		</p>
-		<p class="float-end d-none d-sm-block mb-0">
-			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
-		</p>
-		<p class="text-muted d-none d-sm-block meta-data small mt-2">
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-				<?php
-					$categories_list = get_the_category_list( __( ', ', 'ipt_kb' ) );
-					if ( $categories_list && ipt_kb_categorized_blog() ) :
-				?>
-				<span class="cat-links me-3">
-					<i class="fa-regular fa-folder-open"></i>&nbsp;<?php printf( __( 'Posted in %1$s', 'ipt_kb' ), $categories_list ); ?>
-				</span>
+		<div class="d-flex flex-wrap align-items-center justify-content-between">
+			<div class="text-muted small d-flex flex-wrap gap-3">
+				<?php if ( 'post' == get_post_type() ) : ?>
+					<?php $categories_list = get_the_category_list( __( ', ', 'ipt_kb' ) );
+					if ( $categories_list && ipt_kb_categorized_blog() ) : ?>
+					<span><i class="fa-regular fa-folder-open me-1"></i> <?php printf( __( '%1$s', 'ipt_kb' ), $categories_list ); ?></span>
+					<?php endif; ?>
+					<?php $tags_list = get_the_tag_list( '', __( ', ', 'ipt_kb' ) );
+					if ( $tags_list ) : ?>
+					<span><i class="fa-regular fa-tags me-1"></i> <?php printf( __( '%1$s', 'ipt_kb' ), $tags_list ); ?></span>
+					<?php endif; ?>
 				<?php endif; ?>
-
-				<?php
-					$tags_list = get_the_tag_list( '', __( ', ', 'ipt_kb' ) );
-					if ( $tags_list ) :
-				?>
-				<span class="tags-links me-3">
-					<i class="fa-regular fa-tags"></i> <?php printf( __( 'Tagged %1$s', 'ipt_kb' ), $tags_list ); ?>
-				</span>
-				<?php endif; ?>
-			<?php endif; ?>
-
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="comments-link me-3"><i class="fa-regular fa-comments"></i>&nbsp;<?php comments_popup_link( __( 'Leave a comment', 'ipt_kb' ), __( '1 Comment', 'ipt_kb' ), __( '% Comments', 'ipt_kb' ) ); ?></span>
-			<?php endif; ?>
-			<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '<span class="ms-3"><i class="fa-regular fa-pen-to-square"></i> ', '</span>' ); ?>
-		</p>
-		<div class="clearfix"></div>
+				<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '<span class="ms-3"><i class="fa-regular fa-pen-to-square"></i> ', '</span>' ); ?>
+			</div>
+			<div>
+				<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm"><i class="fa-solid fa-arrow-right me-1"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
+			</div>
+		</div>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
 <div class="clearfix"></div>
