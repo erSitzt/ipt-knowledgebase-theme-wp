9c64cac53c2789e0f36169dd5bd44e524a9e4ea7
Dennis Buehring <1625376+erSitzt@users.noreply.github.com>
2025-05-22 16:05:12 +0200

    looks

--- content-single.php
+++ content-single.php
@@ -5,11 +5,11 @@
 global $ipt_theme_op_settings;
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header page-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+<article id="post-<?php the_ID(); ?>" <?php post_class('mb-5 pb-4 border-bottom'); ?>>
+	<header class="entry-header page-header mb-3">
+		<h1 class="entry-title mb-2 h3"><?php the_title(); ?></h1>
 
-		<div class="entry-meta text-muted">
+		<div class="entry-meta text-muted small d-flex align-items-center gap-3 mb-2">
 			<?php ipt_kb_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
@@ -29,32 +29,23 @@ global $ipt_theme_op_settings;
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta text-muted well well-sm">
+	<footer class="entry-meta text-muted bg-light p-3 rounded small mt-3">
 		<?php
-			/* translators: used between list items, there is a space after the comma */
 			$category_list = get_the_category_list( __( ', ', 'ipt_kb' ) );
-
-			/* translators: used between list items, there is a space after the comma */
 			$tag_list = get_the_tag_list( '', __( ', ', 'ipt_kb' ) );
-
 			if ( ! ipt_kb_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged <i class="glyphicon glyphicon-tags"></i>&nbsp;&nbsp;%2$s. <br />Bookmark the <i class="glyphicon glyphicon-bookmark"></i>&nbsp;&nbsp;<a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
+					$meta_text = __( 'This entry was tagged <i class="fa-regular fa-tags"></i> %2$s. <br />Bookmark the <i class="fa-regular fa-bookmark"></i> <a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
 				} else {
-					$meta_text = __( 'Bookmark the <i class="glyphicon glyphicon-bookmark"></i>&nbsp;&nbsp;<a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
+					$meta_text = __( 'Bookmark the <i class="fa-regular fa-bookmark"></i> <a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
 				}
-
 			} else {
-				// But this blog has loads of categories so we should probably display them here
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in <i class="glyphicon glyphicon-folder-open"></i>&nbsp;&nbsp;%1$s and tagged <i class="glyphicon glyphicon-tags"></i>&nbsp;&nbsp;%2$s. <br />Bookmark the <i class="glyphicon glyphicon-bookmark"></i>&nbsp;&nbsp;<a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
+					$meta_text = __( 'This entry was posted in <i class="fa-regular fa-folder-open"></i> %1$s and tagged <i class="fa-regular fa-tags"></i> %2$s. <br />Bookmark the <i class="fa-regular fa-bookmark"></i> <a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
 				} else {
-					$meta_text = __( 'This entry was posted in <i class="glyphicon glyphicon-folder-open"></i>&nbsp;&nbsp;%1$s. <br />Bookmark the <i class="glyphicon glyphicon-bookmark"></i>&nbsp;&nbsp;<a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
+					$meta_text = __( 'This entry was posted in <i class="fa-regular fa-folder-open"></i> %1$s. <br />Bookmark the <i class="fa-regular fa-bookmark"></i> <a href="%3$s" rel="bookmark">permalink</a>.', 'ipt_kb' );
 				}
-
-			} // end check for categories on this blog
-
+			}
 			printf(
 				$meta_text,
 				$category_list,
@@ -62,7 +53,6 @@ global $ipt_theme_op_settings;
 				get_permalink()
 			);
 		?>
-
-		<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '<span class="edit-link"><i class="glyphicon glyphicon-edit"></i>&nbsp;&nbsp;', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '<span class="edit-link ms-3"><i class="fa-regular fa-pen-to-square"></i> ', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
--- content.php
+++ content.php
@@ -4,12 +4,12 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'kb-article-excerpt' ); ?>>
-	<header class="entry-header page-header">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'kb-article-excerpt mb-5 pb-4 border-bottom' ); ?>>
+	<header class="entry-header page-header mb-3">
+		<h2 class="entry-title h4 mb-2"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta text-muted">
+		<div class="entry-meta text-muted small d-flex align-items-center gap-3 mb-2">
 			<?php ipt_kb_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
@@ -18,12 +18,12 @@
 	<div class="clearfix"></div>
 
 	<?php if ( has_post_thumbnail() ) : ?>
-	<div class="pull-left kb-thumb-medium hidden-xs">
+	<div class="float-start kb-thumb-medium d-none d-sm-block me-3 mb-2">
 		<a rel="bookmark" href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail( 'ipt_kb_medium', array(
-			'class' => 'img-rounded',
+			'class' => 'img-thumbnail rounded',
 		) ); ?></a>
 	</div>
-	<div class="kb-thumb-large visible-xs">
+	<div class="kb-thumb-large d-block d-sm-none mb-2">
 		<a rel="bookmark" href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail( 'ipt_kb_large', array(
 			'class' => 'img',
 		) ); ?></a>
@@ -35,40 +35,38 @@
 	</div>
 
 	<div class="clearfix"></div>
-	<footer class="entry-meta">
-		<p class="visible-xs">
-			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary btn-block"><i class="glyphicon glyphicon-link"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
+	<footer class="entry-meta mt-2">
+		<p class="d-block d-sm-none mb-2">
+			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary btn-block"><i class="fa-solid fa-arrow-right"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
 		</p>
-		<p class="pull-right hidden-xs">
-			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary"><i class="glyphicon glyphicon-link"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
+		<p class="float-end d-none d-sm-block mb-0">
+			<a rel="bookmark" href="<?php the_permalink(); ?>" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i> <?php _e( 'Read more', 'ipt_kb' ); ?></a>
 		</p>
-		<p class="text-muted hidden-xs meta-data">
+		<p class="text-muted d-none d-sm-block meta-data small mt-2">
 			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
 				<?php
-					/* translators: used between list items, there is a space after the comma */
 					$categories_list = get_the_category_list( __( ', ', 'ipt_kb' ) );
 					if ( $categories_list && ipt_kb_categorized_blog() ) :
 				?>
-				<span class="cat-links">
-					<i class="glyphicon glyphicon-folder-open"></i>&nbsp;&nbsp;<?php printf( __( 'Posted in %1$s', 'ipt_kb' ), $categories_list ); ?>
+				<span class="cat-links me-3">
+					<i class="fa-regular fa-folder-open"></i>&nbsp;<?php printf( __( 'Posted in %1$s', 'ipt_kb' ), $categories_list ); ?>
 				</span>
-				<?php endif; // End if categories ?>
+				<?php endif; ?>
 
 				<?php
-					/* translators: used between list items, there is a space after the comma */
 					$tags_list = get_the_tag_list( '', __( ', ', 'ipt_kb' ) );
 					if ( $tags_list ) :
 				?>
-				<span class="tags-links">
-					<i class="glyphicon glyphicon-tags"></i> <?php printf( __( 'Tagged %1$s', 'ipt_kb' ), $tags_list ); ?>
+				<span class="tags-links me-3">
+					<i class="fa-regular fa-tags"></i> <?php printf( __( 'Tagged %1$s', 'ipt_kb' ), $tags_list ); ?>
 				</span>
-				<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
+				<?php endif; ?>
+			<?php endif; ?>
 
 			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="comments-link">&nbsp;&nbsp;<i class="glyphicon ipt-icomoon-bubbles2"></i>&nbsp;<?php comments_popup_link( __( 'Leave a comment', 'ipt_kb' ), __( '1 Comment', 'ipt_kb' ), __( '% Comments', 'ipt_kb' ) ); ?></span>
+			<span class="comments-link me-3"><i class="fa-regular fa-comments"></i>&nbsp;<?php comments_popup_link( __( 'Leave a comment', 'ipt_kb' ), __( '1 Comment', 'ipt_kb' ), __( '% Comments', 'ipt_kb' ) ); ?></span>
 			<?php endif; ?>
-			<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '&nbsp;&nbsp;<i class="glyphicon glyphicon-edit"></i>&nbsp;<span class="edit-link">', '</span>' ); ?>
+			<?php edit_post_link( __( 'Edit', 'ipt_kb' ), '<span class="ms-3"><i class="fa-regular fa-pen-to-square"></i> ', '</span>' ); ?>
 		</p>
 		<div class="clearfix"></div>
 	</footer><!-- .entry-meta -->
