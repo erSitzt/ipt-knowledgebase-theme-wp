68e403b9d55229187674ba715eefc9c21e2d0951
Dennis Buehring <1625376+erSitzt@users.noreply.github.com>
2025-05-22 16:22:41 +0200

    looks

--- index.php
+++ index.php
@@ -44,7 +44,33 @@ get_header(); ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 
-		<?php get_sidebar(); ?>
+		<aside id="secondary" class="widget-area col-md-4 d-print-none">
+			<div class="card mb-4 shadow-sm">
+				<div class="card-header bg-primary text-white d-flex align-items-center">
+					<i class="fa-solid fa-magnifying-glass me-2"></i> <span class="fw-bold">Search</span>
+				</div>
+				<div class="card-body">
+					<?php get_search_form(); ?>
+				</div>
+			</div>
+			<div class="card mb-4 shadow-sm">
+				<div class="card-header bg-success text-white d-flex align-items-center">
+					<i class="fa-solid fa-list me-2"></i> <span class="fw-bold">Categories</span>
+				</div>
+				<div class="card-body">
+					<?php wp_list_categories(array('title_li' => '')); ?>
+				</div>
+			</div>
+			<div class="card mb-4 shadow-sm">
+				<div class="card-header bg-warning text-dark d-flex align-items-center">
+					<i class="fa-solid fa-tags me-2"></i> <span class="fw-bold">Tags</span>
+				</div>
+				<div class="card-body">
+					<?php wp_tag_cloud(); ?>
+				</div>
+			</div>
+			<?php get_sidebar(); ?>
+		</aside>
 	</div><!-- .row -->
 </div><!-- .container -->
 
